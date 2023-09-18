999801225452a37d334169838ad93e43853f1dfa
aleino0 <113008053+aleino0@users.noreply.github.com>
2023-09-18 10:29:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ_query_primjer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> stringList = new List<string>()
+            {
+                "C# Tutorials",
+                "vs.NET Tutorials",
+                "Learn c++",
+                "HVC Tutorials",
+                "Java"
+            };
+
+            var result = from s in stringList
+                         where s.Contains("Tutorials")
+                         select s;
+
+            foreach (var str in result)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
